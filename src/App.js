867fd3d68a0e935bd5924c4a8c345776de53232b
vinnie <vtlam@sfu.ca>
2018-03-25 04:32:58 -0700

    External API search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import Autosuggest from 'react-autosuggest';
+import './App.css';
+
+const API_KEY = "https://api.themoviedb.org/3/search/tv?api_key=5f9a2ab08c36a2b6a3f27847719a4b8a&language=en-US&query=";
+
 
 class App extends Component {
 
@@ -36,16 +40,22 @@ class App extends Component {
 
     // SEARCH BAR IMPLEMENTATION
 
-    getSuggestions = value => {
-        const inputValue = value.trim().toLowerCase();
-        const inputLength = inputValue.length;
+    searchRequest = () => {
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
+        axios.get('/api/movie/search',{params: {title: this.state.searchText}
+            })
+            .then(res => {
+                this.setState({movies: res.data});
+                console.log(this.state.movies);
+            })
+            .catch((error) => {
+                if (error.response.status === 401) {
+                    this.props.history.push("/login");
+                }
+            });
+    }
 
-        return inputLength === 0 ? [] : this.state.movies.filter(movie =>
-            movie.title.toLowerCase().slice(0, inputLength) === inputValue
-        );
-    };
-
-    getSuggestionValue = suggestion => suggestion.name;
+    getSuggestionValue = suggestion => suggestion.title;
 
     renderSuggestion = suggestion => (
         <span>
@@ -53,22 +63,46 @@ class App extends Component {
         </span>
     );
 
-    renderSectionTitle(section) {
-        return (
-            <strong>{section.isbn}</strong>
-        );
-    }
-
     onChange = (event, { newValue }) => {
         this.setState({
             searchText: newValue
         });
     };
 
+    handleSearchSubmit = (event) => {
+        event.preventDefault();
+        this.searchRequest;
+        console.log(this.state.searchText);
+    };
+
     onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            searchSuggestions: this.getSuggestions(value)
-        });
+        const inputValue = value.trim().toLowerCase();
+        const inputLength = inputValue.length;
+        if(inputLength > 0){
+            let url = API_KEY + inputValue;
+
+            fetch(url)
+                .then(response => response.json())
+                .then(json => json.results)
+                .then(data => {
+                    const results = data.map(movie => {
+                        let temp = {}
+                        temp.isbn = movie.id
+                        temp.title = movie.name
+                        temp.img = movie.poster_path
+                        temp.year = (movie.first_air_date == "") ? "0000" : movie.first_air_date.substring(0, 4)
+                        return temp;
+                    });
+                    this.setState({
+                        searchSuggestions: results.slice(0,10)
+                    });
+                });
+        } else {
+            this.setState({
+                searchSuggestions: []
+            });
+        }
+        console.log(this.state);
     };
 
     onSuggestionsClearRequested = () => {
@@ -96,15 +130,19 @@ class App extends Component {
                         {localStorage.getItem('jwtToken') &&
                         <button class="btn btn-primary" onClick={this.logout}>Logout</button>
                         }
-                        <Autosuggest
-                            suggestions={suggestions}
-                            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                            getSuggestionValue={this.getSuggestionValue}
-                            renderSuggestion={this.renderSuggestion}
-                            inputProps={inputProps}
-                        />
                     </h3>
+                    <div class="searchBar">
+                        <form onSubmit={this.handleSearchSubmit}>
+                            <Autosuggest
+                                suggestions={suggestions}
+                                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                                getSuggestionValue={this.getSuggestionValue}
+                                renderSuggestion={this.renderSuggestion}
+                                inputProps={inputProps}
+                            />
+                        </form>
+                    </div>
                 </div>
                 <div class="panel-body">
                     <table class="table table-stripe" id="movie-list">
